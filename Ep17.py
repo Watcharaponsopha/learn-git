959a793d559e73121f4d75c257f0fa1dc0a0aa5a
watchrapon <134664875+Watcharaponsopha@users.noreply.github.com>
2023-06-01 23:12:16 +0700

    add1

---
+++ Ep17.py
@@ -0,0 +1,13 @@
+# pass
+age = int(input("ป้อนอายุของคุณ:"))
+if age<=15:
+    if age==15:
+        pass
+    elif age==14:
+        pass
+    elif age==13:
+        pass
+    else :
+        print("ประถม")
+else :
+    print("ม.ปลาย")
