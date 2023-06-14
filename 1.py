686ea663ddf0118f63bdfca6d02b80964e507119
watchrapon <134664875+Watcharaponsopha@users.noreply.github.com>
2023-06-14 13:55:47 +0700

    a

---
+++ 1.py
@@ -0,0 +1,12 @@
+'''  #Pseudocode #
+SET r = INPUT("Enter the radius of the circle(cm)")
+SET area = π*r*r
+SET circumference = 2*π*r
+OUTPUT("Area of the circle : , area")
+OUTPUT("circumference of the circle  : , circumference " )
+'''
+r= float(input("Enter the radius of the circle(cm):"))
+area = 3.14 *r*r
+circumference = 3.14*2*r
+print("Area of the circle :" ,area )
+print("circumference of the circle  : ", circumference  )
